e2c47b4ba71a00457eb0b0e7937825162c3bf20e
NickSaltamachia <nsaltamachia@gmail.com>
2023-10-05 15:57:07 -0500

    Callback Functions Lab completed

---
+++ script.js
@@ -0,0 +1,179 @@
+
+// Exercise 1
+/*
+function writeDing() {
+    console.log("Ding!");
+}
+    let timerId = setTimeout(writeDing, 3000);
+*/
+
+
+
+
+
+
+/*
+// Exercise 2
+// Write the sorting callback as a named function declaration
+function sortArray(a, b) { }
+// Or
+function sortByLength(a, b) { }
+
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+function sortByLength(a, b) {
+    if (a.length < b.length) {
+        return -1;
+    } else if (a.length < b.length) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+// The sort method sorts "in place", that is, it modifies the array
+words.sort(sortByLength);
+
+// Check that logging words now outputs
+// ["nice", "short", "medium", "lengthy", "delicious"]
+console.log(words);
+*/
+
+
+
+
+
+
+
+
+/*
+// Exercise 3
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+const longWords = words.filter(function (word) {
+    return word.length >= 7;
+});
+console.log(longWords)
+*/
+
+
+
+
+
+
+/*
+// Exercise 4
+// Code a forEach method:
+// 		1. Write a function named forEach.
+//		2. The forEach function accepts two args, an array & a callback.
+//		3. Code the forEach method such that it iterates over each element in the array arg (use a for loop).
+
+//		4. For each iteration, invoke the callback arg, passing to it, the element and the index of the element.
+
+// Test with this array
+const colors = ['red', 'green', 'blue', 'purple'];
+// and this callback
+function log(elem, idx) {
+    console.log(`Index: ${idx} / Element Value: ${elem}`)
+}
+
+function forEach(arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+        cb(arr[i], i);
+    }
+}
+forEach(colors, log);
+*/
+
+
+
+
+
+
+/*
+// Exercise 5
+function step1(cb) {
+  setTimeout(function() {
+    console.log('STEP 1 COMPLETE');
+    cb()
+  }, 750);
+}
+
+function step2(cb) {
+  setTimeout(function() {
+    console.log('STEP 2 COMPLETE');
+    cb()
+  }, 500);
+}
+	
+function step3(cb) {
+  setTimeout(function() {
+    console.log('STEP 3 COMPLETE');
+    cb()
+  }, 250);
+}
+
+/////////////////////////////////////// My solution
+function logFin() {
+    console.log("FINISHED")
+}
+
+function callInOrder() {
+    step1();
+    setTimeout(step2, 251);
+    setTimeout(step3, 502);
+    setTimeout(logFin, 754);
+}
+callInOrder();
+
+*/
+/*
+The above functions are working asynchronous functions - DO NOT
+change any of their code. They are what we call "black boxes"
+because we do not need to know anything that goes on inside of them.
+
+Each of the three functions accept a single argument - a callback function.
+
+Write the code that invokes the three functions such that the output in the console will be:
+
+STEP 1 COMPLETE
+STEP 2 COMPLETE
+STEP 3 COMPLETE
+FINISHED
+
+Hints: 
+- Call `step1` first.
+- You cannot call `step2` until after `step1` has "finished", similarly, you cannot call `step3` until `step2` has "finished".
+- You must console.log the last line of the output, `FINISHED`, after `step3` has "finished".
+*/
+
+
+
+
+
+
+/*
+BONUS
+
+Write function named countdown that accepts as an arg the starting number of seconds and console.logs the count down to zero one second apart from each other.
+
+For example:
+countdown(3);
+
+console.logs something like the following:
+
+Count: 3
+Count: 2
+Count: 1
+Count: 0
+
+
+/////////////////////////////////////// My solution
+function countdown(arg) {
+    console.log(`Count: ${arg}`);
+    
+    if (arg > 0) {
+        setTimeout(function () {
+        countdown(arg - 1);
+        }, 1000)
+    }
+    }
+countdown(3);
+*/
